aa7459c1495b9392c23744f5b3989dc847cfc71d
paul ramain <paulramain44@gmail.com>
2023-06-03 11:44:00 +0200

    update

---
+++ src/Controller/testController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class testController extends AbstractController {
+
+    // #[Route('/' ,'home.index', methods :[GET])]
+    public function index():Response{
+
+        // on retrouve les parametres  dela route dans les attributs de la $request
+
+        // $request = Request::createFromGlobals();
+
+        // $age = $request->query->get('age', 0);
+
+        //$age = $request->attributes->get('age, 0');
+        //$age = 'pd';
+
+        //return new Response ("vous avez $age");
+
+        return $this->render('index.html.twig');
+    }
+
+    public function yolo(Request $request, $age):Response{
+       
+        // $age = $request->attributes->get('age',0);
+
+        return new Response ("2eme page UwU $age");
+    }
+}
