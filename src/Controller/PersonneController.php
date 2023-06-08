e961d6102094871931821ee14e6b78adb5738b94
Paul Ramain <paulramain44@gmail.com>
2023-06-08 19:35:08 +0200

    oui

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -10,13 +10,20 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class PersonneController extends AbstractController
 {
+    //*************** faire un READ avec doctrine ****************
+
     #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
-    {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
-        ]);
+    public function index(ManagerRegistry $doctrine): Response
+    {   
+        // pour faire un select on utilise repository ainsi que l'entité qu'on veut select
+        $repository = $doctrine->getRepository(Personne::class);
+        // On peut ensuite utiliser les differentes methodes de repository sur l'entité ciblé
+        $personnes = $repository->findAll();
+        
+        return $this->render('personne/index.html.twig', ['personnes' => $personnes]);
     }
+
+    //  ****************  faire un INSERT avec doctrine ****************
 
     public function addPersonne(ManagerRegistry $doctrine): Response{
 
@@ -32,6 +39,37 @@ class PersonneController extends AbstractController
 
         // execute la transaction
         $entityManager->flush();
-        return $this->render('/personne/index.html.twig', ['controller_name' => 'PersonneController',]);
+        return $this->render('/personne/add.html.twig', ['controller_name' => 'PersonneController',]);
+    }
+
+    // ****************** read une personne et gestion de l'id *********************
+    public function detail(ManagerRegistry $doctrine, $id): Response {
+
+        $repository = $doctrine->getRepository(Personne::class);
+        $personne = $repository->find($id);
+
+        if(!$personne){
+            $this->addFlash('error','l\'id n\'existe pas');
+            return $this->redirectToRoute('personne');
+        }
+        return $this->render('/personne/detail.html.twig', ['personne' => $personne]);
+    }
+
+    public function indexAll(ManagerRegistry $doctrine, $page, $nb): Response {
+
+        $repository = $doctrine->getRepository(Personne::class);
+        $nbPersonne = $repository->count([]);
+        $nbPage = ceil($nbPersonne / $nb); // nombre d'entrée ds la bdd  (ici des personnes) divisé par la limit
+
+
+        // findBy permet de search avec 4 parametres (name,id puis ASC/DESC puis limit en enfin offset)
+        $personnes = $repository->findBy([],[],$nb,($page - 1) * $nb);
+
+        return $this->render('/personne/index.html.twig',[
+            'personnes' => $personnes,
+            'nbPage' => $nbPage,
+            'page' => $page,
+            'nb' => $nb
+        ]);
     }
 }
